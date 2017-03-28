abf4551383596560bd7248a950a27638c2f5fed5
krammohan <kavya.ramm@gmail.com>
2017-03-28 15:39:25 -0700

    Completes edit and delete functinoality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,42 @@ class ArticlesController < ApplicationController
     @category = Category.find(params[:category_id])
   end
 
+  def create
+  	@category = Category.find(params[:category_id])
+  	@article = Article.new(article_params)
+  	@article.category_id = @category.id
+  	@article.save
+  	redirect_to "/categories/#{@category.id}/articles"
+  end
+
+  def show
+  	@article = Article.find(params[:id])
+  	@category = @article.category
+  end
+
+  def edit
+  	@article = Article.find(params[:id])
+  	@category = @article.category
+  end
+
+  def update
+    @article = Article.find(params[:id])
+  	@category = @article.category
+    @article.update_attributes(article_params)
+    redirect_to "/categories/#{@category.id}/articles/#{@article.id}"
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @category = @article.category
+    @article.destroy
+    redirect_to "/categories/#{@category.id}/articles"
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,4 +8,19 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
   end
 
+  def new
+  	@category = Category.new
+  end
+
+  def create
+  	@category = Category.create(category_params)
+  	redirect_to '/'
+  end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
+
 end
